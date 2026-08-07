0432a45e1eb31e1e9753dec83b8f758100de0fc2
恋上东雪莲爆米138 <13574875397@163.com>
2026-08-07 12:54:08 +0800

    更新配置文件

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -76,8 +76,8 @@ export const siteConfig: SiteConfig = {
 		// 使用 Astro 图标库时不需要设置 valueDark，图标会自动跟随主题亮暗色切换
 		logo: {
 			type: "image",
-			value: "assets/images/logo/alice-light.jpg",
-			valueDark: "assets/images/logo/alice-dark.jpg",
+			value: "assets/images/logo/alice-light.gif",
+			valueDark: "assets/images/logo/alice-dark.gif",
 			alt: "🍟",
 		},
 		// 导航栏标题
